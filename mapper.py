c33fa58f9c6f5cc409d11c5b317a771b1c1415f4
lookp20 <63848656+lookp20@users.noreply.github.com>
2020-11-21 16:39:22 +0100

    Update mapper.py
    Update der clean Funktion

--- mapper.py
+++ mapper.py
@@ -3,37 +3,21 @@ import sys
 import re
 
 def clean(word):
-    if '\xc3\x84' in word:
-        return word.replace('\xc3\x84', 'ae')
-    elif '\xc3\x96' in word:
-        return word.replace('\xc3\x96', 'oe')
-    elif '\xc3\x9c' in word:
-        return word.replace('\xc3\x9c', 'ue')
-    elif '\xc3\x9f' in word:
-        return word.replace('\xc3\x9f', 'ss')
-    elif '\xc3\xa4' in word:
-        return word.replace('\xc3\xa4', 'ae')
-    elif '\xc3\xb6' in word:
-        return word.replace('\xc3\xb6', 'oe')
-    elif '\xc3\xbc' in word:
-        return word.replace('\xc3\xbc', 'ue')
-    elif '\xc5\xbf' in word:
-        return word.replace('\xc5\xbf', 's')
-    elif 'u\xcd\xa4' in word:
-        return word.replace('u\xcd\xa4', 'ue')
-    elif 'a\xcd\xa4' in word:
-        return word.replace('a\xcd\xa4', 'ae')
-    elif 'o\xcd\xa4' in word:
-        return word.replace('o\xcd\xa4', 'oe')
-    elif '\xc5\xbf' in word:
-        return word.replace('\xc5\xbf', 's')
-    elif 'U\xcd\xa4' in word:
-        return word.replace('U\xcd\xa4', 'ue')
-    elif 'A\xcd\xa4' in word:
-        return word.replace('A\xcd\xa4', 'ae')
-    elif 'O\xcd\xa4' in word:
-        return word.replace('O\xcd\xa4', 'oe')
-        
+    word = word.replace('\xc3\x84', 'ae')
+    word = word.replace('\xc3\x96', 'oe')
+    word = word.replace('\xc3\x9c', 'ue')
+    word = word.replace('\xc3\x9f', 'ss')
+    word = word.replace('\xc3\xa4', 'ae')
+    word = word.replace('\xc3\xb6', 'oe')
+    word = word.replace('\xc3\xbc', 'ue')
+    word = word.replace('\xc5\xbf', 's')
+    word = word.replace('u\xcd\xa4', 'ue')
+    word = word.replace('a\xcd\xa4', 'ae')
+    word = word.replace('o\xcd\xa4', 'oe')
+    word = word.replace('\xc5\xbf', 's')
+    word = word.replace('U\xcd\xa4', 'ue')
+    word = word.replace('A\xcd\xa4', 'ae')
+    word = word.replace('O\xcd\xa4', 'oe')    
     return word
 
 def main():
