a007e5d6b29c375798bab1fc35b86fd03779d635
lookp20 <63848656+lookp20@users.noreply.github.com>
2020-11-22 20:29:49 +0100

    Update text_collection.sh

--- text_collection.sh
+++ text_collection.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-docker cp /Users/lookphanthavong/Documents/VisualStudioCode/BDEA/flask/text_collection/. d14632e1dd51:/src/Flask_textcollection
-cmd='/bin/bash -c "hdfs dfs -put /src/Flask_textcollection/*.txt /user/history/Flask/Flask_textcollection"'
-docker_exec="docker exec -t d14632e1dd51 $cmd"
-eval $docker_exec
-cmd1='/bin/bash -c "rm /src/Flask_textcollection/*.txt"'
-docker_exec1="docker exec -t d14632e1dd51 $cmd1"
-eval $docker_exec1
+docker cp /Users/lookphanthavong/Documents/VisualStudioCode/BDEA/flask/text_collection/. 4c0d173b1e74:/src/Flask_textcollection
+docker exec -t 4c0d173b1e74 /bin/bash -c "hdfs dfs -put /src/Flask_textcollection/*.txt /user/history/Flask_textcollection"
+docker exec -t 4c0d173b1e74 /bin/bash -c "rm -r /src/Flask_textcollection/*.txt"
 rm /Users/lookphanthavong/Documents/VisualStudioCode/BDEA/flask/text_collection/*.txt
