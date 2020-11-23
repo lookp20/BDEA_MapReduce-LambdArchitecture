844157498028c0f3d3baa2590076920144c85d70
lookp20 <63848656+lookp20@users.noreply.github.com>
2020-11-23 14:45:19 +0100

    Add files via upload

---
+++ delete_BATCH.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -r /Users/lookphanthavong/Documents/VisualStudioCode/BDEA/flask/static/img_for_BATCH/*
+rm -r /Users/lookphanthavong/Documents/VisualStudioCode/BDEA/flask/static/text_for_wc_batch/*
